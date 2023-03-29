6dbe1e73a115bcb22c7424fa4c9bfa1de7f18237
Ruslan Hristov <rusi@me.com>
2023-03-28 21:51:42 -0400

    changed plugin ID based on obsidian guidelines.

--- release.sh
+++ release.sh
@@ -4,6 +4,6 @@ set -o pipefail
 npm run build
 npm version patch
 # npm run release
+git push origin master --tags
 version=$(cat manifest.json | jq -r ".version")
 gh release create ${version} -F CHANGELOG.md manifest.json main.js styles.css
-git push
